f82cfc7857d065ac7bb7770e22a05ca0734105f6
benji290402 <benjamin.vialsimon@ynov.com>
2021-11-30 12:01:33 +0100

    Update multiplication-table.php

--- multiplication-table.php
+++ multiplication-table.php
@@ -1,9 +1,9 @@
 <?php
 $result = 0;
-$multipl = 1;
+$multipl = 0;
 while ($result<90) {
     $result = $result + 9;
     $multipl ++;
-    echo"$multipl * 9 = $result";
+    echo"$multipl * 9 = $result \n";
 }
 ?>
