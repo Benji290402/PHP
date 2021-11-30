6f55d95c6456996b892e9174cd118c4f4d228f00
benji290402 <benjamin.vialsimon@ynov.com>
2021-11-30 10:02:00 +0100

    Update primitive-declarations.php

--- primitive-declarations.php
+++ primitive-declarations.php
@@ -1,12 +1,11 @@
 <?php 
 
-
-$firstStr=" Hello my name is Frank";
+$firstStr="Hello my name is Frank";
 $firstEscapeStr="Dam son !!!\n You're a bench !";
 $firstBool = true;
 $firstInteger = 26;
 $firstFloat = 26.25;
 $firstNull = null;
-$firstArray = array(2, null, 'hi',5.5, false);
+$firstArray = array(1=>2, 2=>null, 3=>'hi',4=>5.5, 5=>false);
 
 ?>
