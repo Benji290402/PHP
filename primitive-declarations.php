cb2cc70c36f460781cca368e5756e5953fea35eb
benji290402 <benjamin.vialsimon@ynov.com>
2021-11-30 09:52:43 +0100

    Create primitive-declarations.php

---
+++ primitive-declarations.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+$firstStr=" Hello my name is Frank";
+$firstEscapeStr="Dam son !!!\n You're a bench !";
+$firstBool = true;
+$firstInteger = 26;
+$firstFloat = 26.25;
+$firstNull = null;
+$firstArray = array(2, null, 'hi',5.5, false);
+
+?>
