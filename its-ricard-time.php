17773987127dd66222e3c526182aeef475f6dd8f
benji290402 <benjamin.vialsimon@ynov.com>
2021-12-07 09:25:16 +0100

    Update its-ricard-time.php

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -1,7 +1,7 @@
 <?php
     $nbrRicard = 0;
     do {
-        $nbrRicard++;
+        
         echo"Come on ricard number $nbrRicard \n";
         if($nbrRicard = 3){
             echo "I'll have to stop soon!\n";
@@ -12,5 +12,6 @@
         if($nbrRicard = 7){
             break;
         }
+        $nbrRicard++;
     }while($nbrRicard < 9);
 ?>
