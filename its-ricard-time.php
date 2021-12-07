33768dcc06af9513266fa5d2823ebbf76816908b
benji290402 <benjamin.vialsimon@ynov.com>
2021-12-07 09:25:46 +0100

    Update its-ricard-time.php

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -3,13 +3,13 @@
     do {
         
         echo"Come on ricard number $nbrRicard \n";
-        if($nbrRicard = 3){
+        if($nbrRicard == 3){
             echo "I'll have to stop soon!\n";
         }
-        if($nbrRicard = 5){
+        if($nbrRicard == 5){
             echo"I am no longer very fresh...\n";
         }
-        if($nbrRicard = 7){
+        if($nbrRicard == 7){
             break;
         }
         $nbrRicard++;
