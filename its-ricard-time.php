11c120ed911b294e62e9a76d9151c0c0531b0d0c
benji290402 <benjamin.vialsimon@ynov.com>
2021-12-07 09:17:41 +0100

    Create its-ricard-time.php

---
+++ its-ricard-time.php
@@ -0,0 +1,14 @@
+<?php
+    $nbrRicard = 0;
+    do {
+        $nbrRicard++;
+        echo"Come on ricard number $nbrRicard \n";
+        if($nbrRicard = 3){
+            echo "I'll have to stop soon!\n";
+        }
+        if($nbrRicard = 5){
+            echo"I am no longer very fresh...\n";
+        }
+        
+    }while($nbrRicard<7);
+?>
