b80e306bbd1f1f3457fea36a532bc8368ff5254d
benji290402 <benjamin.vialsimon@ynov.com>
2021-12-14 11:56:55 +0100

    Update say-hello-to-functions.php

--- say-hello-to-functions.php
+++ say-hello-to-functions.php
@@ -2,7 +2,8 @@
     function sayHello() {
         return "Hello";
     }
-    function sayHelloTo( string $name){      
+    function sayHelloTo( string $name):string
+    {      
         return "Hello ".$name;
     }
 
